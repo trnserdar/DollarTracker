aac7997bab6faafe1aa26694e7019d9dba45e140
TURKCELL\TCSERDTURAN <turan.serdar@turkcell.com.tr>
2021-01-23 16:38:12 +0300

    Initial Commit

---
+++ DollarTracker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DollarTracker
+//
+//  Created by SERDAR TURAN on 23.01.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
