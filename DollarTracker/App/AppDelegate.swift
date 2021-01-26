edd91f4408a7e19eb6db7589772415ed4a1b08f7
trnserdar <trnserdar@gmail.com>
2021-01-26 09:58:50 +0300

    improvements about app delegate and added app delegate test.

--- DollarTracker/App/AppDelegate.swift
+++ DollarTracker/App/AppDelegate.swift
@@ -15,10 +15,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var preferencesItem: NSMenuItem!
     
     var statusItem: NSStatusItem?
-
+    let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
+    var timer: Timer?
+    
+    var localeManager: LocaleManagerProtocol = LocaleManager()
+    var networkManager: NetworkManagerProtocol = NetworkManager()
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
 
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(configurePreferences), name: Notification.Name("PreferencesChanged"), object: nil)
+        
+        configurePreferences()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -42,6 +50,59 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     @IBAction func showPreferences(_ sender: NSMenuItem) {
         
+        guard let preferencesViewController = storyboard.instantiateController(withIdentifier: PreferencesViewController.storyboardId) as? PreferencesViewController,
+              statusItem?.button != nil else {
+            return
+        }
+        
+        let popoverView = NSPopover()
+        popoverView.contentViewController = preferencesViewController
+        popoverView.behavior = .transient
+        popoverView.show(relativeTo: statusItem!.button!.bounds, of: statusItem!.button!, preferredEdge: .maxY)
+    }
+    
+    @objc func configurePreferences() {
+    
+        timer = Timer.scheduledTimer(timeInterval: 60 * 30, target: self, selector: #selector(getLatest), userInfo: nil, repeats: true)
+        timer?.tolerance = 60
+        
+        getLatest()
+    }
+    
+    @objc func getLatest() {
+        
+        guard let apiKey = localeManager.getApiKey() else {
+            statusItem?.button?.title = "Please enter api key"
+            return
+        }
+        
+        let selectedCurrency = localeManager.getSelectedCurrencyType()
+        let displayMode = localeManager.getDisplayMode()
+        var title = ""
+        
+        networkManager.getLatest(apiKey: apiKey, selectedCurrency: selectedCurrency) { (response) in
+            defer {
+                DispatchQueue.main.async {
+                    self.statusItem?.button?.title = title
+                }
+            }
+            
+            guard let response = response else {
+                title = "Please enter valid api key"
+                return
+            }
+
+            if let rates = response[selectedCurrency] as? Double {
+                title = displayMode == 0 ? "1 USD = \(String(format: "%.2f", rates)) \(selectedCurrency)" : "1 \(selectedCurrency) = \(String(format: "%.2f", 1 / rates)) USD"
+            }
+            
+            if let lastUpdate = response["timestamp"] as? Int {
+                let date = Date(timeIntervalSince1970: Double(lastUpdate))
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "HH:mm"
+                title += " \(dateFormatter.string(from: date))"
+            }
+        }
     }
     
 
--- DollarTrackerTests/Cases/App/AppDelegateTests.swift
+++ DollarTrackerTests/Cases/App/AppDelegateTests.swift
@@ -11,20 +11,72 @@ import XCTest
 class AppDelegateTests: XCTestCase {
 
     var sut: AppDelegate!
+    var localeManager: MockLocaleManager!
+    var networkManager: MockNetworkManager!
     
     override func setUp() {
         super.setUp()
         sut = AppDelegate()
+        networkManager = MockNetworkManager()
+        sut.networkManager = networkManager
+        localeManager = MockLocaleManager()
+        sut.localeManager = localeManager
         sut.awakeFromNib()
     }
     
     override func tearDown() {
         sut = nil
+        networkManager = nil
+        localeManager = nil
         super.tearDown()
     }
     
     func test_initAppDelegate_setStatusItemTitle() {
         XCTAssertEqual(sut.statusItem?.button?.title, "Dollar Tracker")
     }
+    
+    func test_getLatest_whenApiKeyEmpty_setStatusItemTitle() {
+        localeManager.setValue(value: nil, key: .apiKey)
+        sut.getLatest()
+        XCTAssertEqual(sut.statusItem?.button?.title, "Please enter api key")
+    }
 
+    func test_getLatest_whenResponseEmpty_setStatusItemTitle() {
+        let expectationLatest = expectation(description: "Latest")
+        localeManager.setValue(value: "test", key: .apiKey)
+        sut.getLatest()
+        networkManager.latestCompletion?(nil)
+        DispatchQueue.main.async {
+            expectationLatest.fulfill()
+        }
+        wait(for: [expectationLatest], timeout: 0.1)
+        XCTAssertEqual(sut.statusItem?.button?.title, "Please enter valid api key")
+    }
+    
+    func test_getLatest_whenGivenResponse_whenDisplayOption0_setStatusItemTitle() {
+        let expectationLatest = expectation(description: "Latest")
+        localeManager.setValue(value: "test", key: .apiKey)
+        localeManager.setValue(value: "EUR", key: .selectedCurrencyType)
+        sut.getLatest()
+        networkManager.latestCompletion?(["timestamp": 1611399603, "EUR": 0.821389])
+        DispatchQueue.main.async {
+            expectationLatest.fulfill()
+        }
+        wait(for: [expectationLatest], timeout: 0.1)
+        XCTAssertEqual(sut.statusItem?.button?.title, "1 USD = 0.82 EUR 14:00")
+    }
+    
+    func test_getLatest_whenGivenResponse_whenDisplayOption1_setStatusItemTitle() {
+        let expectationLatest = expectation(description: "Latest")
+        localeManager.setValue(value: "test", key: .apiKey)
+        localeManager.setValue(value: "EUR", key: .selectedCurrencyType)
+        localeManager.setValue(value: 1, key: .displayMode)
+        sut.getLatest()
+        networkManager.latestCompletion?(["timestamp": 1611399603, "EUR": 0.821389])
+        DispatchQueue.main.async {
+            expectationLatest.fulfill()
+        }
+        wait(for: [expectationLatest], timeout: 0.1)
+        XCTAssertEqual(sut.statusItem?.button?.title, "1 EUR = 1.22 USD 14:00")
+    }
 }
